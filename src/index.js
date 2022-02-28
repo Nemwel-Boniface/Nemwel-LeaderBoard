7c5128fc92e70c9d53d1f4b8861d0b9b205d4593
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-02-28 07:02:09 +0300

    Update included add functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,44 @@
 import _ from 'lodash';
 import './style.css';
 
+const leaderBoardWrapper = document.querySelector('.leaders');
+const leaderName = document.querySelector('#name');
+const leaderScore = document.querySelector('#number');
+const addLeader = document.querySelector('#add');
+const form = document.querySelector('#form');
+
+let myLeaderBoard = [
+  {
+    id: 1,
+    name: 'Nemwel',
+    score: 100,
+  },
+  {
+    id: 2,
+    name: 'Boniface',
+    score: 80,
+  },
+];
+
+const addLeaderToList = () => {
+  let index = myLeaderBoard.length;
+  myLeaderBoard.push({
+    id: index + 1,
+    name: leaderName.value,
+    score: leaderScore.value,
+  });
+  addToLocalStorage();
+  displayLeader();
+  leaderName.value = '';
+  leaderScore.value = '';
+};
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  addLeaderToList();
+  addToLocalStorage();
+})
+
+window.addEventListener('load', () => {
+  displayLeader();
+})
