d58a67762ad606e802644d9a1454dc7f3647e0f2
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-03-02 20:53:19 +0300

    Update changes to display function

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const leaderName = document.querySelector('#name');
 const leaderScore = document.querySelector('#number');
 const form = document.querySelector('#form');
 
-const myGameURL = 'ojq3rdgoTgouE5sLha5K';
+const myGameURL = 'aJjPvQ7cnENMtVlki6KE';
 export const baseURL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${myGameURL}/scores/`;
 
 form.addEventListener('submit', (e) => {
@@ -19,7 +19,7 @@ form.addEventListener('submit', (e) => {
 });
 
 export const displayLeaders = ({ user, score }) => {
-  leaderBoardWrapper.innerHTML += `<li><span>${user}</span><span class="span">${score}</span></li>`;
+  leaderBoardWrapper.innerHTML += `<li class="animated bounce"><span>${user}</span><span class="span">${score}</span></li>`;
 };
 
 document.addEventListener('click', async (click) => {
