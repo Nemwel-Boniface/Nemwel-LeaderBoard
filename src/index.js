ba9e1a5725089e37ec1b79e78e018f972ef0d4df
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-03-02 07:47:23 +0300

    Update include the retrieve from API functionality

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const leaderBoardWrapper = document.querySelector('.leaders');
 const leaderName = document.querySelector('#name');
 const leaderScore = document.querySelector('#number');
 const form = document.querySelector('#form');
+
 const myGameURL = 'ojq3rdgoTgouE5sLha5K';
 const baseURL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${myGameURL}/scores/`;
 
@@ -27,4 +28,21 @@ form.addEventListener('submit', (e) => {
   e.preventDefault();
   postToApi(leaderName.value, leaderScore.value);
   form.reset();
+});
+
+
+const retireveFromAPI = async () => {
+  const res = await fetch(baseURL);
+  const text = await res.json();
+  // displayLeaders(text);
+  console.log(text);
+}
+
+const refresh = document.querySelector('#refresh');
+
+document.addEventListener('click', (click)=> {
+  if(click.target.id === 'refresh') {
+    console.log('touched')
+    retireveFromAPI();
+  }
 });
