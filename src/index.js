f1140061fd4d3ed617376fcc8ec948d2ac520997
Nemwel Boniface <nemwelnyandoro20@gmail.com>
2022-03-02 06:47:32 +0300

    Update add to API complete functionality

--- src/index.js
+++ src/index.js
@@ -4,35 +4,27 @@ const leaderBoardWrapper = document.querySelector('.leaders');
 const leaderName = document.querySelector('#name');
 const leaderScore = document.querySelector('#number');
 const form = document.querySelector('#form');
-// const myGameURL = 'ojq3rdgoTgouE5sLha5K';
-// const baseURL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/ojq3rdgoTgouE5sLha5K/scores/`;
+const myGameURL = 'ojq3rdgoTgouE5sLha5K';
+const baseURL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${myGameURL}/scores/`;
 
-const gameID = 'Y4TJzkMfBJeirD2R1Zn7';
-const baseURL = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameID}/scores/`;
 
 const postToApi = async (name, score) => {
-  console.log('called')
-  const data = await fetch(baseURL, {
+  await fetch(baseURL, {
     method: 'POST',
-    body: JSON.stringify({
-      name,
-      score,
-    }),
     headers: {
       'Content-type': 'application/json; charset=UTF-8',
     },
-  });
-  console.log('finished')
-  const json = data.json();
-  return json;
+    body: JSON.stringify({
+      user: name,
+      score: score,
+    }),
+  })
+  .then((response) => response.json())
+  .then((json) => console.log(json));
 }
 
-const post = () => {
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    console.log('default');
-    postToApi(leaderName.value, leaderScore.value);
-    form.reset();
-  })
-}
-post();
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  postToApi(leaderName.value, leaderScore.value);
+  form.reset();
+});
